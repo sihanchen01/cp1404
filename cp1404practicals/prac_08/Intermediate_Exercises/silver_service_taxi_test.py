373765b1c88107181b049d9c558a13126efd3f40
sihan <sihanchen01>
2022-03-12 13:19:41 +0800

    remove extra files

--- cp1404practicals/prac_08/Intermediate_Exercises/silver_service_taxi_test.py
+++
@@ -1,8 +0,0 @@
-from silver_service_taxi import SilverServiceTaxi
-
-hummer = SilverServiceTaxi(name='Hummer', fuel=100, fanciness=4)
-print(str(hummer))
-
-silver = SilverServiceTaxi(name='silver taxi', fuel=100, fanciness=2)
-silver.drive(18)
-print(silver.get_fare())
--- cp1404practicals/prac_08/Intermediate_Exercises/unreliable_car_test.py
+++
@@ -1,8 +0,0 @@
-from unicodedata import name
-from unreliable_car import UnreliableCar
-
-unreliable = UnreliableCar(name='Camry_unreliable', fuel=100, reliability=10)
-
-unreliable.drive(10)
-unreliable.drive(10)
-unreliable.drive(10)
