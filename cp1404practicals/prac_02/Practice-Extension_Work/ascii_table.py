38a91a4c7a7ca6b2e755e3fa145930c9725e4491
sihan <sihanchen01>
2022-03-25 11:03:43 +0800

    redid ascii table

--- cp1404practicals/prac_02/Practice-Extension_Work/ascii_table.py
+++ cp1404practicals/prac_02/Practice-Extension_Work/ascii_table.py
@@ -4,15 +4,15 @@ UPPER = 127
 
 def main():
     char = input("Enter a character: ")
-    while ord(char) < 65 or 90 < ord(char) < 97 or ord(char) > 122:
+    while ord(char) < LOWER or ord(char) > UPPER:
         print("Invalid character! Try again")
         char = input("Enter a character: ")
     print(f"The ASCII code for {char} is {ord(char)}")
 
-    num = int(input("Enter a number between 33 and 127: "))
+    num = int(input(f"Enter a number between {LOWER} and {UPPER}: "))
     while num < LOWER or num > UPPER:
         print("Invalid number! Try again")
-        num = int(input("Enter a number between 33 and 127: "))
+        num = int(input(f"Enter a number between {LOWER} and {UPPER}: "))
     print(f"The character for {num} is {chr(num)}")
 
     for i in range(LOWER, UPPER + 1):
