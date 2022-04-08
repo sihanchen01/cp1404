0cccea7ee0c0394c600960203ccaaae18cbaba6d
sihan <sihanchen01>
2022-04-08 10:21:50 +0800

    refactor get_incomes

--- cp1404practicals/prac_04/Walkthrough_Example/total_income.py
+++ cp1404practicals/prac_04/Walkthrough_Example/total_income.py
@@ -7,16 +7,21 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
+
+    get_incomes(incomes)
+    print_report(incomes)
+
+
+def get_incomes(incomes):
     month_counts = int(input("How many months? "))
 
     for month in range(1, month_counts + 1):
         income = float(input("Enter income for month " + str(month) + ": "))
         incomes.append(income)
 
-    print_report(incomes)
-
 
 def print_report(incomes):
+    """print incomes by month."""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(0, len(incomes)):
