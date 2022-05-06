6859c58d351f4ff5029f3353c9b6735ecb383a33
sihan <sihanchen01>
2022-05-06 09:17:06 +0800

    change doc string & author

--- cp1404practicals/prac_07/Intermediate_Exercises/squaring.py
+++ cp1404practicals/prac_07/Intermediate_Exercises/squaring.py
@@ -1,15 +1,15 @@
 """
 CP1404/CP5632 Practical
 Kivy GUI program to square a number
-Lindsay Ward, IT@JCU
-Started 13/10/2015
+Sihan Chen, IT@JCU
+Started May.06.2022
 """
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Sihan Chen'
 
 
 class SquareNumberApp(App):
